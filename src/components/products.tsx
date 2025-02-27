5a17c331852ab4659b65feab8a9a0f58c2223cfe
ozankersit <ozankersit@gmail.com>
2025-02-28 00:47:11 +0300

    nextjs prerender error fix

---
+++ src/components/products.tsx
@@ -0,0 +1,32 @@
+import ProductCard, {
+  ProductProps,
+} from "@/components/product-card/product-card";
+
+export default async function Products() {
+  const WEB_URL = process.env.NEXT_LOCAL_WEB_URL;
+  async function getProducts(): Promise<ProductProps[]> {
+    const res = await fetch(`http://localhost:3000/api/products`, {
+      next: { revalidate: 30 },
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return data.products;
+  }
+  const data = await getProducts();
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 mt-5">
+      {data.map((item) => (
+        <ProductCard
+          id={item.id}
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          description={item.description}
+          imageUrl={item.imageUrl}
+        />
+      ))}
+    </div>
+  );
+}
