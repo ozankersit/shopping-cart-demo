0e390a7263765b8966ec7dcbdba67e23562d3e72
ozankersit <ozankersit@gmail.com>
2025-03-03 20:14:48 +0300

    products loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/header/header";
 import Providers from "@/providers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Products from "@/components/products";
+import SkeletonLoader from "@/components/skeleton-loader/skeleton-loader";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <Products/>
+    <Suspense fallback={<SkeletonLoader/>}>
+      <Products/>
+    </Suspense>
+    
   );
 }
