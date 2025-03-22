e8cfb5c64d982144881dc0312b80f13020f005bb
ozankersit <ozankersit@gmail.com>
2025-03-22 21:37:44 +0300

    removed unused fallback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import Products from "@/components/products";
-import SkeletonLoader from "@/components/skeleton-loader/skeleton-loader";
-import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <Suspense fallback={<SkeletonLoader/>}>
       <Products/>
-    </Suspense>
-    
   );
 }
